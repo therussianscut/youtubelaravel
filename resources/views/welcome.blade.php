d7ef1b72b44cb8ff9f3f113fe960358b937c6479
Sean McCann <smccann@athium.com>
2019-07-30 16:47:23 +0100

    sign in page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,78 @@
 
 
 
-@section('container')
+@section('content')
 
 
-    <h2> Main Page</h2>
+    <div class="row">
+
+
+        <div class="col-md-6">
+
+            <h3>Sign up</h3>
+
+            <form method="post" action="">
+
+                <div class="form-group">
+
+                    <label for="email"> Your E-mail</label>
+                    <input class="form-control" type="email" name="email" id="email">
+
+
+                    <label for="first_name">First Name</label>
+                    <input  class='form-control' type="text" name="first_name" id="first_name">
+
+                    <label for="password">Password</label>
+                    <input  class='form-control' type="password" name="password" id="password">
+
+
+                    <button class="btn btn-primary" type="submit"> Submit</button>
+                </div>
+
+
+
+
+
+
+            </form>
+
+
+
+
+        </div>
+
+        <div class="col-md-6">
+
+            <h3> Sign in</h3>
+
+            <form method="post" action="">
+
+                <div class="form-group">
+
+                    <label for="email"> Your E-mail</label>
+                    <input class="form-control" type="email" name="email" id="email">
+
+
+                    <label for="password">Password</label>
+                    <input  class='form-control' type="password" name="password" id="password">
+
+
+                    <button class="btn btn-primary" type="submit"> Sign in</button>
+                </div>
+
+
+
+
+
+
+            </form>
+
+
+
+        </div>
+
+        </div>
+
 
 
 
